6cab6e7af4a514e6eb2966b09d2bb957e3f4b06d
rajministore <akritijaiswal528@gmail.com>
2026-07-16 22:52:54 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,10 @@
-js
+const SUPABASE_URL = "https://kpswfoxhfnqcqfiybttb.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_UM1grvRRMosVkHtBh-D74g_JnYEjj5P";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+console.log("Supabase Connected");
