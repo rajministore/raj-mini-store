bd676a26c0c79777ca477967b39b66d74b5395cd
rajministore <akritijaiswal528@gmail.com>
2026-07-16 22:57:46 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,39 @@
-j
+document.addEventListener("DOMContentLoaded", () => {
+
+const btn = document.querySelector("button");
+const inputs = document.querySelectorAll("input");
+
+btn.addEventListener("click", async () => {
+
+const name = inputs[0].value;
+const price = inputs[1].value;
+const image = inputs[2].value;
+
+if(!name || !price || !image){
+alert("Sabhi fields bharo");
+return;
+}
+
+const { error } = await supabase
+.from("products")
+.insert([
+{
+name: name,
+price: price,
+image_url: image
+}
+]);
+
+if(error){
+alert("Error: " + error.message);
+console.log(error);
+}
+else{
+alert("Product Add Ho Gaya");
+inputs[0].value = "";
+inputs[1].value = "";
+inputs[2].value = "";
+}
+});
+
+});
