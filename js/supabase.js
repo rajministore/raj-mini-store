f13acdd2fbdc564ff6d6ca548ba54014b93fbe6b
rajministore <akritijaiswal528@gmail.com>
2026-07-17 23:21:34 +0530

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL =
+"https://kpswfoxhfnqcqfiybttb.supabase.co";
+
+const SUPABASE_ANON_KEY =
+"sb_publishable_UM1grvRRMosVkHtBh-D74g_JnYEjj5P";
+
+const supabase = window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_ANON_KEY
+);
